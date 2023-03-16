c8cd5bd27e31a8aae1ee695dae47c169ee1ed151
april0114 <119941414+april0114@users.noreply.github.com>
2023-03-16 16:23:38 +0900

    Update 5597.py

--- 4번 배열/5597.py
+++ 4번 배열/5597.py
@@ -7,3 +7,13 @@ for i in range(N):
 for i in range(1,N+1):
   if i not in list1:
     print('%d' % i)
+    //내가 풀었던것
+
+N = 28
+list1 = [int(input()) for i in range(28)]
+
+for i in range(1,31):
+  if i not in list1:
+    print('%d' % i)
+    
+    //참조해서 푼것
