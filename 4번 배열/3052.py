df37b239c864e0ce961f8281646f80c0a3c8af77
april0114 <119941414+april0114@users.noreply.github.com>
2023-03-17 14:00:22 +0900

    Update 3052.py

--- 4번 배열/3052.py
+++ 4번 배열/3052.py
@@ -3,7 +3,7 @@ a = []
 for _ in range(10):
   b = int(input())
   c = b % 42
-  a.append(c)
+  a.append(c)    //append= 추가
 
-s = set(a)
-print(len(s))
+s = set(a)       //set = 중복요소 제거해줌
+print(len(s))    // list안에 갯수 세줌
